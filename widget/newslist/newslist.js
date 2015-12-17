14ee84033b47eec127ee3dd3a54808160556195e
sophia <sophiaytyt@gmail.com>
2015-12-17 22:13:17 +0800

    fix

--- widget/newslist/newslist.js
+++ widget/newslist/newslist.js
@@ -1,5 +1,19 @@
 var $ = require('jquery');
+var Template = require('template');
 var app = module.exports = function(opt) {
+	var list = {
+		init : function(){
+			this.listTemplate = __inline('./newslist.tpl');
+			this.addEvent();
+		},
+		addEvent : function(){
+			var _this = this;
+			$(opt.dom).click(function(){
+				console.log(_this.listTemplate);
+			});
+		}
+	};
+	list.init();
 	$(opt.dom).click(function(){
 		$("#mask").css('height', document.body.scrollHeight).show();
 	})
