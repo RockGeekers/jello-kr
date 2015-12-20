ff01bb84f4b9d58670231cb7c8d664002b468dcf
sophia <sophiaytyt@gmail.com>
2015-12-20 18:44:23 +0800

    comment

--- widget/detail/detail.js
+++ widget/detail/detail.js
@@ -1,14 +1,52 @@
 var $ = require('jquery');
-var app = module.exports = function(opt) {
+var Template = require('template');
+var app = module.exports = function(opt){
 	var detail = {
 		init : function(){
+			this.commentTemplate = __inline('./detail.tpl');
 			this.addEvent();
 		},
 		addEvent : function(){
-			// if(loginInfo){
-			// 	console.log(loginInfo);
-			// }
+			var _this = this;
+			var getlogin = setInterval(function(){
+				if(loginInfo){
+					clearInterval(getlogin);
+					if(loginInfo.isUserLogin){
+						$('#J_userInfo').find('.avatar').css('backGroundImage',loginInfo.image);
+						$('.require-login').show();
+						$('.J_requirelogin').hide();
+					}else{
+						$('.require-login').hide();
+						$('.J_requirelogin').show();
+					}
+					_this.getList(opt.commentUrl,{
+						author_id : loginInfo.id
+					},'get',function(data){
+						if(data.code == 0){
+							if(data.data.length){
+								var result = Template.parse(_this.commentTemplate,{data:data});
+								$('#J_comments').html(result);
+							}
+						}
+					});
+				}
+			},100);
+			$('#J_comments').delegate('.pull-right','click',function(){
+
+			});
 		},
+		getList : function(url,opt,type,callback){
+			var _this = this;
+			$.ajax({
+			   	type: type,
+			   	url: url,
+			   	data: opt,
+			   	success: callback
+			}).complete(function(){
+				console.log('success');
+			});
+		} 
 	};
 	detail.init();
-};
+}
+
