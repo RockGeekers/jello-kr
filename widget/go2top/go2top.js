f7f6fb0618e1e08593345c1f294592fd233ea1fa
sophia <sophiaytyt@gmail.com>
2015-12-21 21:25:39 +0800

    fix

--- widget/go2top/go2top.js
+++ widget/go2top/go2top.js
@@ -1,26 +1,21 @@
-var $ = require('common:jquery');
+var $ = require('jquery');
 
-! function(a, b, c) {
-		function d() {
-			var a = 0 !== h.length ? h.height() : null,
-				b = 0 !== i.length ? i.offset().top + i.height() : null,
-				c = a || b;
-			return c && c > 50 ? c : 250
-		}
-		var e = a("#J_backTop .j_small-rcode"),
-			f = a("#J_backTop .j_back-top"),
-			g = a("#J_backTop .j_hf-app-content"),
-			h = a("#header"),
-			i = a("#lpDetailNav");
-		e.mouseover(function() {
-			g.show()
-		}).mouseout(function() {
-			g.hide()
-		}), f.click(function() {
-			c.body.scrollTop ? c.body.scrollTop = 0 : c.documentElement.scrollTop = 0
-		}), a(b).scroll(function() {
-			var a = c.body.scrollTop || c.documentElement.scrollTop,
-				b = d();
-			a > b ? f.removeClass("hide") : f.addClass("hide")
-		})
-}($ || jQuery, window, document);
+var e = $("#J_backTop .j_small-rcode"),
+	f = $("#J_backTop .j_back-top"),
+	g = $("#J_backTop .j_hf-app-content"),
+	h = $("#header"),
+	i = $("#lpDetailNav");
+function d() {
+	var a = 0 !== h.length ? h.height() : null,
+		b = 0 !== i.length ? i.offset().top + i.height() : null,
+		c = a || b;
+	return c && c > 50 ? c : 250
+}
+f.click(function() {
+	document.body.scrollTop ? document.body.scrollTop = 0 : document.documentElement.scrollTop = 0
+});
+$(window).scroll(function() {
+	var a = document.body.scrollTop || document.documentElement.scrollTop,
+		b = d();
+	a > b ? f.removeClass("hide") : f.addClass("hide")
+});
