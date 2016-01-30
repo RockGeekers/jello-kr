2bb2468b4c9024d961f2388aa6e7b7eb855e04e2
杨婷 <yangting393@pingan.com.cn>
2016-01-30 16:37:15 +0800

    fix

--- components/tween/tween.js
+++ components/tween/tween.js
@@ -23,20 +23,57 @@ function Tween(opt){
 		props : {},
 		duration : 1000,
 		easing : 'lining',
+		loop : false,
 		callback : function(){}
 	},opt || {});
 
 	this.init();
 }
 Tween.prototype.init = function(){
-	var _this = this;
+	var self = this, opt = self.options || {};
+
+	self._queueProps = [];
+	self._initProps = [];
+	self._queueAnimate = [];
+	self._currentProps = [];
+	self._target = opt.dom ;
 	this.core();
 };
 Tween.prototype.addAnimation = function(argument){
 	 // body...  
 };
-Tween.prototype.setProperties = function(args){
+Tween.prototype.addProps = function(props){
+	var self = this, opt = self.options || {},
+		props = props ? props : opt.props || {},
+		oldprop;
+
+	for(var p in props){
+		if(self._initProps[p] === undefined){
+			oldprop = self._target[p];
+			if(p in Tween.cssPropMap){
+				oldprop = self.getStyleProp(p,value); //当前属性值 
+			}
+			self._initProps = self._currentProps = (oldValue === undefined) ? null : oldprop;
+		} else {
+			oldprop = self.currentProps[p]; 
+		}
+		self.queueProps[p] = props[p];
+	}
+	return self.queueProps;
+};
+Tween.prototype.setProperties = function(props){
 	document.getElementById("animation").style.opacity=args;
+};
+Tween.prototype.getStyleProp = function(prop,value){
+	var map = Tween.cssPropMap,
+		self = this,
+		_target = self._target,
+		style,unit;
+	if((unit = map[prop]) == null || !(style = _target.style)) { return value;}
+	var propv = _target.currentStyle ? _target.currentStyle[prop] ? getComputedStyle(_target,null)[prop] ;
+	if(!propv) return 0;
+	var i = propv.length - unit.length;
+	return parseInt(propv.substr(0,i));
 };
 Tween.prototype.run = function(argument){
 	 // body...  
@@ -56,4 +93,6 @@ Tween.prototype.core = function(){
             }
 		}((i/10)), i * 50);
 	};
-}
+};
+
+Tween.cssPropMap = {top:"px",left:"px",bottom:"px",right:"px",width:"px",height:"px",opacity:""}
